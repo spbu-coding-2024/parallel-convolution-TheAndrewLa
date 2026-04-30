4a9d35092cdf42eecf5f0bab029ea622f373ceb1
Andrew Lazarev <andrew.lzrv@gmail.com>
2026-04-30 22:40:22 +0300

    feat: Add parallel processor
    Signed-off-by: Andrew Lazarev <andrew.lzrv@gmail.com>

---
+++ src/main/java/andrewla/processors/ParallelSingleProcessor.java
@@ -0,0 +1,11 @@
+package andrewla.processors;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+public class ParallelSingleProcessor extends BaseSingleProcessor {
+    @Override
+    public List<BufferedImage> applyFilters() {
+        return List.of();
+    }
+}
